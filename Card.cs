bcd652968a1a0f3583942083131842e68a503a89
Stefan Bettaglio <sbettaglio@gmail.com>
2020-02-14 07:49:28 -0500

    Explorer, first draft

---
+++ Card.cs
@@ -0,0 +1,30 @@
+namespace BlackJack
+{
+  public class Card
+  {
+    public string Rank { get; set; }
+    public string Suit { get; set; }
+
+    public string DisplayCard()
+    {
+      return $"{Rank} of {Suit}";
+    }
+    public int GetCardValue()
+    {
+      if (Rank == "ace")
+      {
+        return 11;
+      }
+      else if (Rank == "king" || Rank == "queen" || Rank == "jack")
+      {
+        return 10;
+      }
+      else
+      {
+        return int.Parse(Rank);
+      }
+    }
+
+  }
+
+}
